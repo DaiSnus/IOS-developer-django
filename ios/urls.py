ab15ad013cd64fb0f5194a1a87bce65fbe881b16
DaiSnus <nikita.kozlov.200516@gmail.com>
2025-01-14 19:17:06 +0500

    First migrations, configuring the router, adding data via the admin panel and making layout.

--- ios/urls.py
+++ ios/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', LandingPage.as_view(), name="home"),
+    path('general/', GeneralPageView.as_view(), name="general"),
+    path('top-skills/', TopSkillsPage.as_view(), name="top-skills"),
+    path('relevance/', RelevancePage.as_view(), name="relevance"),
+    path('regional/', RegionalPage.as_view(), name="geography"),
+    path('skills/', SkillsPage.as_view(), name="skills")
 ]
--- ios_developer/settings.py
+++ ios_developer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ios_developer',
+    'ios'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,10 @@ ROOT_URLCONF = 'ios_developer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+            os.path.join(BASE_DIR, 'ios_developer', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +113,8 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
+DEFAULT_CHARSET = 'utf-8'
+
 USE_I18N = True
 
 USE_TZ = True
@@ -116,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
